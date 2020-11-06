702d5dbeb4c8027493354ac2fa8270ddf334e235
Sean Rivard Morton <srivardmorton-admin@clap-mac24.mynet>
2020-11-06 12:19:08 +0000

    basic auth with auth0

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import "../styles/globals.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 function MyApp({ Component, pageProps }) {
-  console.log("test");
-  return <Component {...pageProps} />;
+  return (
+    <Auth0Provider
+      domain="chapterific.eu.auth0.com"
+      clientId="Iitf46uMtWldl9Orcg03ibtmeEkXlXWB"
+      redirectUri="http://localhost:3000"
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,14 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import { LoginButton, LogoutButton, Profile } from "../components/Login";
 
 export default function Home() {
-  return <div className={styles.container}>books are for nerds</div>;
+  return (
+    <div>
+      <LoginButton></LoginButton>
+      <LogoutButton></LogoutButton>
+      <Profile></Profile>
+      sup
+    </div>
+  );
 }
