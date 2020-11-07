2e54126b9b4fa0d645e12a41460aadcb4f322083
Sean Rivard Morton <srivardmorton-admin@clap-mac24.mynet>
2020-11-07 22:08:26 +0000

    api stuff

--- hooks/useApi.ts
+++ hooks/useApi.ts
@@ -1,47 +1,76 @@
-import React from "react";
+import {
+  useQuery,
+  useMutation,
+  useQueryCache,
+  QueryCache,
+  ReactQueryCacheProvider,
+  queryCache,
+} from "react-query";
 import { useAuth0 } from "@auth0/auth0-react";
 
-export const useApi = (url, options = {}) => {
+const wishUrl =
+  "https://j9ogf83xx7.execute-api.eu-west-2.amazonaws.com/default/wish-list-service";
+
+const params: any = {
+  audience: `https://auth0-jwt-authorizer`,
+  scope: "read:current_user update:current_user_metadata",
+};
+
+const token =
+  "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlRmQmdGZXE1ODd4YlA2c0JpaWhOZiJ9.eyJpc3MiOiJodHRwczovL2NoYXB0ZXJpZmljLmV1LmF1dGgwLmNvbS8iLCJzdWIiOiJnb29nbGUtb2F1dGgyfDEwNTQwNzM2NTY1ODM1NjEyNTEzNCIsImF1ZCI6WyJodHRwczovL2F1dGgwLWp3dC1hdXRob3JpemVyIiwiaHR0cHM6Ly9jaGFwdGVyaWZpYy5ldS5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNjA0Nzg1MTgzLCJleHAiOjE2MDQ4NzE1ODMsImF6cCI6IklpdGY0NnVNdFdsZGw5T3JjZzAzaWJ0bWVFa1hsWFdCIiwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCJ9.mbSHxDKANZrx0QXxYkfNQavYx65-v7Zx6otiFzHuKQpufAqHPu4Nh2mLDCO_fvWQbIWNEtGetV3815aibd9iQtDxKUlA3cCg3Eqj6N930HGTs88OkklmA2Gjqw6ZjUlMcIjai0hdafiXev2Ff7qxkiV0Jnv66svDZueHqM6x1QjxOxYiheneBkc2-PGosIZAkhdhR4MxwcmoA2F3aGgeWNDS0gJC0VO6dHf28aAfxqayyolMCM1kP6YQsR68S2dVwIGhlbevaI5a9Nnijgiv40nfIounpleLu017O-kzx02kfi9FA_N4E0q5qJVmouLe4624Bo_5RTcJ816xnWKNeQ";
+
+export const useWishList = () => {
   const { getAccessTokenSilently } = useAuth0();
-  const [state, setState] = React.useState({
-    error: null,
-    loading: true,
-    data: null,
+
+  return useQuery("posts", async () => {
+    // const accessToken = await getAccessTokenSilently({
+    //   audience: `https://auth0-jwt-authorizer`,
+    //   scope: "read:current_user update:current_user_metadata",
+    // });
+    return fetch(wishUrl, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((res: any) => res.json());
   });
-  const [refreshIndex, setRefreshIndex] = React.useState(0);
+};
 
-  React.useEffect(() => {
-    (async () => {
-      try {
-        const { audience, scope, ...fetchOptions }: any = options;
-        const accessToken = await getAccessTokenSilently({ audience, scope });
-        const res = await fetch(url, {
-          ...fetchOptions,
-          headers: {
-            ...fetchOptions.headers,
-            // Add the Authorization header to the existing headers
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
-        console.log(accessToken);
-        setState({
-          ...state,
-          data: await res.json(),
-          error: null,
-          loading: false,
-        });
-      } catch (error) {
-        setState({
-          ...state,
-          error,
-          loading: false,
-        });
-      }
-    })();
-  }, [refreshIndex]);
+export const useDeleteWishList = () => {
+  const { getAccessTokenSilently } = useAuth0();
 
-  return {
-    ...state,
-    refresh: () => setRefreshIndex(refreshIndex + 1),
-  };
+  return useMutation(
+    async (id) => {
+      //   const accessToken = await getAccessTokenSilently({
+      //     audience: `https://auth0-jwt-authorizer`,
+      //     scope: "read:current_user update:current_user_metadata",
+      //   });
+      return fetch(wishUrl, {
+        method: "DELETE",
+        body: JSON.stringify({ id }),
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }).then((res: any) => res.json());
+    },
+    {
+      onSuccess: () => queryCache.refetchQueries("wishlists"),
+    }
+  );
+};
+
+export const useCreateWishList = () => {
+  return useMutation(
+    async (values) => {
+      return fetch(wishUrl, {
+        method: "POST",
+        body: JSON.stringify(values),
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }).then((res) => res.json());
+    },
+    {
+      onSuccess: () => queryCache.refetchQueries("wishlists"),
+    }
+  );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,26 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import { LoginButton, LogoutButton, Profile } from "../components/Login";
-import { useApi } from "../hooks/useApi";
-import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
-
-const test = async () => {
-  const req = await fetch(
-    "https://j9ogf83xx7.execute-api.eu-west-2.amazonaws.com/default/wish-list-service",
-    {
-      method: "POST",
-      body: JSON.stringify({
-        name: "1",
-        descriptions: "2",
-        url: "3",
-      }),
-    }
-  );
-  const data = await req.json();
-  console.log(data);
-};
+import { useWishList, useDeleteWishList } from "../hooks/useApi";
 
 export default function Home() {
-  const { loading, error, refresh, data: users } = useApi(
-    "https://j9ogf83xx7.execute-api.eu-west-2.amazonaws.com/default/wish-list-service",
-    {
-      method: "GET",
-      audience: `https://auth0-jwt-authorizer`,
-      scope: "read:current_user update:current_user_metadata",
-      // body: JSON.stringify({
-      //   name: "test",
-      //   description: "noooo",
-      //   url: "silly string",
-      // }),
-    }
-  );
-
-  // React.useEffect(() => {
-  //   refresh();
-  // }, []);
-
-  console.log("loading", loading);
-  console.log("data", users);
+  const { data } = useWishList();
+  const [deleteFn] = useDeleteWishList();
 
   return (
     <div>
       <LoginButton></LoginButton>
       <LogoutButton></LogoutButton>
-      <button onClick={() => refresh()}>hey</button>
-      <button onClick={() => test()}>i am dumb</button>
       <Profile></Profile>
+      <ul>
+        {data?.Items.map((i) => {
+          return (
+            <li key={i.id}>
+              <button onClick={() => deleteFn(i.id)}>delete</button>
+              {i.description}
+            </li>
+          );
+        })}
+      </ul>
       sup
     </div>
   );
