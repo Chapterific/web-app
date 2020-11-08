e71b904dea08f95c3e037ed4980b26da615ca881
Sean Rivard Morton <srivardmorton-admin@clap-mac24.mynet>
2020-11-08 14:38:31 +0000

    added material & styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,36 @@
 import "../styles/globals.css";
-import { Auth0Provider } from "@auth0/auth0-react";
+import { createMuiTheme, ThemeProvider, Button } from "@material-ui/core";
 import dynamic from "next/dynamic";
+import styled, { ThemeProvider as StyleProvider } from "styled-components";
 
 const NoSSRComponent = dynamic(() => import("../components/Root"), {
   ssr: false,
 });
 
+const AppBackground = styled.div`
+  ${({ theme }) => `
+    background-color:  ${theme.palette.background.default};
+    height: 100vh;
+  `}
+`;
+
 function MyApp({ Component, pageProps }) {
+  const theme = createMuiTheme({
+    palette: {
+      type: "dark",
+    },
+  });
   // (Sean Rivard-Morton) [2020-11-06] Need to disable SSR for this route for
   // auth0 redirect. Not that it matters, as we're exporting a static application anyway.
-  return <NoSSRComponent Component={Component} pageProps={pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <StyleProvider theme={theme}>
+        <AppBackground>
+          <NoSSRComponent Component={Component} pageProps={pageProps} />
+        </AppBackground>
+      </StyleProvider>
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,10 @@
-import { LoginButton, LogoutButton, Profile } from "../components/Login";
-import React from "react";
-import {
-  useWishList,
-  useDeleteWishList,
-  useCreateWishList,
-} from "../hooks/useApi";
-import { useForm } from "react-hook-form";
+import { Button } from "@material-ui/core";
 
 export default function Home() {
-  const { data } = useWishList();
-  const [deleteFn] = useDeleteWishList();
-  const { register, handleSubmit } = useForm();
-  const [updateWishList] = useCreateWishList();
-  const onSubmit = (data) => updateWishList(data);
   return (
     <div>
-      <LoginButton></LoginButton>
-      <LogoutButton></LogoutButton>
-      <Profile></Profile>
-      <ul>
-        {data?.Items.map((i) => {
-          return (
-            <li key={i.id}>
-              <button onClick={() => deleteFn(i.id)}>delete</button>
-              {i.name}-{i.description}
-            </li>
-          );
-        })}
-      </ul>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input name="name" ref={register} />
-        <input name="description" ref={register} />
-        <input name="url" ref={register} />
-        <button type="submit">submit</button>
-      </form>
-      sup
+      stuff
+      <Button>hey</Button>s
     </div>
   );
 }
