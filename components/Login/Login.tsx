bd69a272efe45d0792fa3736ab704a112fdcc3c9
Sean Rivard Morton <srivardmorton-admin@clap-mac24.mynet>
2020-11-09 21:39:26 +0000

    messing with useApi

--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -3,7 +3,10 @@ import { Typography } from "@material-ui/core";
 import { LoginButton } from "./";
 
 export const Login = ({ children }) => {
-  const { isAuthenticated } = useAuth0();
+  const { isAuthenticated, isLoading } = useAuth0();
+  // (Sean Rivard-Morton) [2020-10-09] TODO
+  // Replace with better loading component
+  if (isLoading) return <div>Loading</div>;
   if (isAuthenticated) return children;
   return (
     <>
